6928400cf97cd140cbcca663159d3adf60085d9a
MK-SWE <magdy_mohammed2000551@outlook.com>
2024-07-11 23:30:41 +0000

    feat: create new docker container from the codeEngine base image

---
+++ backend/execengine/README.md
@@ -0,0 +1,8 @@
+# ExecEngine
+
+```
+The ExecEngine is a simple, lightweight, and efficient execution engine for running client code in seperated enviroment (sandbox). It is designed to provide a secure and isolated enviroment for running untrusted code.
+```
+
+### Features:
+    - new_container() -> str: Create a new container and return the container ID
---
+++ backend/execengine/execEngine.py
@@ -0,0 +1,20 @@
+import subprocess
+
+class docker:
+    """The class manages the code execution logic
+    new_container() -> str: Create a new container and return the container ID
+    """
+    containers = set()
+
+    @staticmethod
+    def new_container() -> str:
+        """Create a new container
+            @Returns: str represent the container ID
+        """
+        try:
+            container_id = subprocess.run(['docker', 'run', '-d', '-it', 'base:latest'], stdout=subprocess.PIPE).stdout.decode().strip()[:12]
+            docker.containers.add(container_id)
+            return container_id
+        except Exception as e:
+            return str(e)
+
